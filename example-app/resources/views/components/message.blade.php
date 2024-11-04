425abe2c359810ce2ce6ec6710456168ade0078e
R.Nishi <wexriexst@gmail.com>
2024-11-04 18:07:30 +0900

    updeta_propsmessage

--- example-app/resources/views/components/message.blade.php
+++ example-app/resources/views/components/message.blade.php
@@ -1,7 +1,8 @@
 <div>
+    @props(['message'])
     @if ($message)
-    <div class="p-4 m-2 rounded bg-green-100">
-        {{ $message }}
-    </div>
+        <div class="p-4 m-2 rounded bg-green-100">
+            {{ $message }}
+        </div>
     @endif
 </div>
